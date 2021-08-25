2d927b02a2b6174b3b340c560707c42e49f6cdbb
301-05\User <boyboy1962@gmail.com>
2021-08-25 21:00:12 +0900

    수요일 2교시 수업
    ★☆ 수업 설명 내용 ☆★
    JDBC 드라이버
    으앙 못 들음
    
    MyBatis 프레임워크, hibernate
    많이 사용되고 이걸 알면 JPA를 쉽게 배울 수 있다.
    아직도 진짜 많이 쓰이고 있다.
    
    JPA
    대세이다.
    
    이번에 DB 연동이다.
    
    1. SpringExampleApplication.java 라인 추가
    설명 주석 및 필요한 import와 필요 없는 import를 정리하였다.
    여기서 중요한 점은 @Bean을 이용한 DB를 사용할 수 있게 준비를 하였다.
    이것 저것이 추가 되는 잘 확인하여 추가하자.
    
    2. plication.properties 라인 추가
    DB를 사용하기 위한 라인이 추가 되었다.
    여기서는 DB가 사용되는 포트넘버와 그리고 DB이름을 11번라인 처럼 작성해야한다.
    10번 주석을 참고하자.
    마지막으로 username과 해당 유저네임의 비번을 적어줘서 자동적으로 로그인 할 수 있게 준비
    
    3. albumMapper.xml 신규 작업중
    DB를 사용하기 위한 xml이다.
    여기에서는 mybatis의 문법이 사용이 되는데 좀더 확인을 해봐야 할 것으로 보인다.

--- src/main/java/com/example/SpringExampleApplication.java
+++ src/main/java/com/example/SpringExampleApplication.java
@@ -1,18 +1,38 @@
 package com.example;
 
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
 //@ComponentScan(basePackages={"com", "com.example", "com.marobiana"})
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+//@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class}) // 이게 데이타 소스는 보지 않게다 해서 DB는 찾지 않는다.
+@MapperScan(basePackages = "com.example.*") // mapper scan 추가!!!
 @SpringBootApplication
+@ComponentScan(basePackages = "com.marobiana")
 public class SpringExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+	@Bean //객채이다. 보통은 설정 파일로 동작하는 객채... 설정
+	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+		sessionFactory.setDataSource(dataSource);
+		
+		//Mapper의 Table 단위로 써야한다.
+		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml"); // 약속된 경로이기에 무족건 이렇게 써야한다.
+		sessionFactory.setMapperLocations(res);
+
+		return sessionFactory.getObject();
 	}
 
 }
