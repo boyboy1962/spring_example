7c0bc3ec72a8d60f73ac6bd77313b5aea2694da4
301-05\User <boyboy1962@gmail.com>
2021-08-27 22:01:18 +0900

    금요일 마지막 교시 수업

--- src/main/java/com/example/lesson03/ReviewRestController.java
+++ src/main/java/com/example/lesson03/ReviewRestController.java
@@ -17,10 +17,11 @@ public class ReviewRestController {
 	//요청 url: http://localhost/lesson03/ex01?id=#
 	@RequestMapping("/lesson03/ex01")
 	public Review ex01(
-			//@RequestParam(value="id") int id						// 필수 파라미터 	id를 빼고 요청을 하면 에러가 난다.
-			//@RequestParam(value="id", required = true) int id		// 필수 파라미터
-			//@RequestParam("id") int id							// 필수 파라미터
-			@RequestParam(value = "id", required = false) Integer id// 비-필수 파라미터 
+			//@RequestParam(value="id") int id										// 필수 파라미터 	id를 빼고 요청을 하면 에러가 난다.
+			//@RequestParam(value="id", required = true) int id						// 필수 파라미터
+			//@RequestParam("id") int id											// 필수 파라미터
+			//@RequestParam(value = "id", required = false) Integer id				// 비-필수 파라미터
+			@RequestParam(value = "id", required = false, defaultValue = "1") int id// 비 필수 파라미터	하지만 디폴트로 숫자가 정해저 있다.
 			) {
 		System.out.println("### id = " + id);
 		Review review = reviewBO.getReviewById(id);
