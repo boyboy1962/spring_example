a26f8a82b8aadcd015e9a6b418662145db857077
301-05\User <boyboy1962@gmail.com>
2021-08-31 21:57:09 +0900

    화요일 3교시 수업 및 숙제
    이야 DB Insert는 과제란다. 헐 ㅋㅋ (NewUserController.java의 28번째 라인)
    
    1. NewUserController.java 라인추가
    일단 ex01/1은 get 방식으로 쓰고 있다.(라인 14)
    그냥 여기서는 jsp파일은 바로 띄워준다고 생각하면 된다.
    그리고 2번에서 post로 보내는 내용을 2번째 method가 받는다
    여기에서는 2번에서 보내주는 모든 파라미터를 가져오고
    DB에 저장을 하고 (이것이 숙제임....)
    마무리 메세지를 띄워준다.
    
    2. addUser.jsp 수정 및 라인추가
    여기는 1번에서 사용하는 jsp파일이다.
    간단하게 이름, 생년월일, 이메일 주소, 자기소개를 작성할 수 있는 작성란을 만들고 (라인 20 ~ 30)
    submit 버튼으로 해당되는 모든 내용을 post로 보낼 수 있게 준비한다. ( 라인 19, 32)
    
    3. lesson04\afterAddUser.jsp 신규
    여기는 1번에서 인서트 이후 마무리를 지을때 여기로 온다.
    딱히 하는 일은 없고 그냥 유져에게 잘했다고 알려주는 용도이다.
    
    4. lesson01\ex02.jsp 라인추가
    실수로 필요하지 않는 부트스트랩을 더했다.
    의미는 없다.

--- src/main/java/com/example/lesson04/NewUserController.java
+++ src/main/java/com/example/lesson04/NewUserController.java
@@ -1,16 +1,33 @@
 package com.example.lesson04;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RequestMapping("/lesson04")
 @Controller
 public class NewUserController {
 	
 	// 요청 URL : http://localhost/lesson04/ex01/1
-	@RequestMapping("/ex01/1")
+	@RequestMapping(method = RequestMethod.GET, path = "/ex01/1") // 이것은 get 방식이다.
 	public String ex01_1() {
-		return "lesson04/addUser"; // /WEB-INF/jsp/		lesson04/addUser	.jsp
+		return "lesson04/addUser"; // /WEB-INF/jsp/		lesson04/addUser	.jsp 
+	}
+	
+	// 요청 URL : http://localhost/lesson04/ex01/add_user
+	//@RequestMapping(method = RequestMethod.POST, path = "/ex01/add_user") // 이것이 post 방식이다. 
+	@PostMapping("/ex01/add_user") // 이것이 더 간단한 post 방식이다. 
+	public String addUser(
+				@RequestParam("name") String name,
+				@RequestParam("yyyymmdd") String yyyymmdd,
+				@RequestParam("email") String email,
+				@RequestParam(value = "introduce", required = false) String introduce // 비필수라서 값이 들어오지 않을 수 있다.
+			) {
+		// DB Insert
+		
+		return "lesson04/afterAddUser";
 	}
 	
 }
